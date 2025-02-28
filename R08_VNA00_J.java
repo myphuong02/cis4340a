3add8b93be0e702b020a5418638d5d59c1467284
myphuong02 <my102095@ucf.edu>
2025-02-27 22:46:33 -0500

    Update R08_VNA00_J.java

--- R08_VNA00_J.java
+++ R08_VNA00_J.java
@@ -1,5 +1,6 @@
+
 final class ControlledStop implements Runnable {
-  private boolean done = false;
+  private volatile boolean done = false;
   
   @Override public void run() {
     while (!done) {
