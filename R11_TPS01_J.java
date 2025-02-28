cad4f769fd0d01636ac5daba66bc70c3f8b40932
myphuong02 <my102095@ucf.edu>
2025-02-27 22:59:49 -0500

    Update R11_TPS01_J.java

--- R11_TPS01_J.java
+++ R11_TPS01_J.java
@@ -1,60 +1,35 @@
 public final class ValidationService {
-  private final ExecutorService pool;
- 
-  public ValidationService(int poolSize) {
-    pool = Executors.newFixedThreadPool(poolSize);
-  }
- 
-  public void shutdown() {
-    pool.shutdown();
-  }
- 
+  // ...
   public StringBuilder fieldAggregator(String... inputs)
       throws InterruptedException, ExecutionException {
- 
-    StringBuilder sb = new StringBuilder();
-    // Stores the results
-    Future<String>[] results = new Future[inputs.length];
- 
-    // Submits the tasks to thread pool
+    // ...
     for (int i = 0; i < inputs.length; i++) {
-      results[i] = pool.submit(
-        new ValidateInput<String>(inputs[i], pool));
+      // Don't pass-in thread pool 
+      results[i] = pool.submit(new ValidateInput<String>(inputs[i]));
     }
- 
-    for (int i = 0; i < inputs.length; i++) { // Aggregates the results
-      sb.append(results[i].get());
-    }
-    return sb;
+    // ...
   }
 }
  
+// Does not use same thread pool
 public final class ValidateInput<V> implements Callable<V> {
   private final V input;
-  private final ExecutorService pool;
  
-  ValidateInput(V input, ExecutorService pool) {
+  ValidateInput(V input) {
     this.input = input;
-    this.pool = pool;
   }
  
   @Override public V call() throws Exception {
     // If validation fails, throw an exception here
-    // Subtask
-    Future<V> future = pool.submit(new SanitizeInput<V>(input));
-    return (V) future.get();
+    return (V) new SanitizeInput().sanitize(input);
   }
 }
  
-public final class SanitizeInput<V> implements Callable<V> {
-  private final V input;
+public final class SanitizeInput<V> {  // No longer a Callable task
+  public SanitizeInput() {}
  
-  SanitizeInput(V input) {
-    this.input = input;
-  }
- 
-  @Override public V call() throws Exception {
+  public V sanitize(V input) {
     // Sanitize input and return
-    return (V) input;
+    return input;
   }
 }
